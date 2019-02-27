3a5bf9191097b47d951080dd91c7a8b636d9dcaa
sierput <sierputx@gmail.com>
2019-02-27 17:58:27 +0100

    pierwszy program

---
+++ ProjektNaAP/src/com/ap/mateusz/Main.java
@@ -0,0 +1,14 @@
+package com.ap.mateusz;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String jeden = "Cos";
+		String dwa = "Cos";
+		String trzy = new String("OOO");
+		String cztery = new String("OOO");
+		if (jeden.equals(dwa))
+			System.out.println("true");
+	}
+
+}
